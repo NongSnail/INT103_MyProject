5037647d673db906abe4f1b9616f1b58b2a59e03
NongFai <narisara.li@mail.kmutt.ac.th>
2020-04-20 17:14:20 +0700

    [UPDATE] Customer

--- src/Person/Customer.java
+++ src/Person/Customer.java
@@ -10,5 +10,28 @@ package Person;
  * @author User
  */
 public class Customer {
-   
+   private long idCard;
+   private String name;
+   private long phoneNumber;
+
+   public Customer(long idCard, String name, long phoneNumber){
+       this.idCard = idCard;
+       this.name = name;
+       this.phoneNumber = phoneNumber;
+   }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Customer other = (Customer) obj;
+        return this.idCard == other.idCard;
+    }
 }
