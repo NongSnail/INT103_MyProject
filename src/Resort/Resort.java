03f28ee88a9a0cc3b1a04914bd6ed6401402ed93
RewBugLag <rewrew2000@gmail.com>
2020-04-24 16:09:24 +0700

    [UPDATE] Resort.finder methods

--- src/Resort/Resort.java
+++ src/Resort/Resort.java
@@ -5,10 +5,61 @@
  */
 package Resort;
 
+import java.util.logging.Logger;
+
+import Person.Administrator;
+import Person.Customer;
+
 /**
  *
  * @author User
  */
-public class Resort {
+public class Resort implements ServiceableResort {
+
+    private String resortName;
+    private Customer customers[];
+    private Administrator admin;
+    private Room rooms[];
+    private Logger resortLogger;
+
+    public Resort(String resortName, Administrator admin, int maximumRoom) {
+        
+    }
+
+    @Override
+    public boolean checkIn(Customer c, RoomType t) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean checkOut(Customer c, int roomNumber) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public int findForAvailableRoom(RoomType t) {
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i].getRoomType() == t) {
+                if (rooms[i].getCustomer() == null) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int findForSpecifiedRoom(int roomNumber) {
+        for (int i = 0; i < rooms.length; i++) {
+            if (rooms[i].getRoomNumber() == roomNumber) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void logHistory() {
+        
+    }
     
 }
